ab119543a5f816eb40547237560f34f1bf341970
Arctie <91278128+Arctie@users.noreply.github.com>
2026-02-05 20:47:59 +0100

    Add gallery images and dynamic gallery controller

---
+++ js/gallery_controller.js
@@ -0,0 +1,54 @@
+//populate as you add more to the Pictures folder! alt is alt text
+
+const threeImages = [
+{src: 'Assets/Pictures/three/ModelShowcase1.png', alt: 'model', title: 'model'}, 
+{src: 'Assets/Pictures/three/ORB1.png', alt: 'model', title: 'model'}, 
+{src: 'Assets/Pictures/three/ORB2.png', alt: 'orb', title: 'orb'}, 
+{src: 'Assets/Pictures/three/ORB3.png', alt: 'orb', title: 'orb'}, 
+{src: 'Assets/Pictures/three/ORB4.png', alt: 'orb', title: 'orb'}, 
+{src: 'Assets/Pictures/three/ORB5.png', alt: 'orb', title: 'orb'}
+
+]
+const twoImages = [
+    {src: 'Assets/Pictures/two/Dergonpanceek.png', alt: 'model', title: 'model'},
+    {src: 'Assets/Pictures/two/KeyniFinal.png', alt: 'model', title: 'model'},
+    {src: 'Assets/Pictures/two/kipp chickenrun child.png', alt: 'model', title: 'model'},
+    {src: 'Assets/Pictures/two/Kipp Kunstrulett.png', alt: 'model', title: 'model'},
+    {src: 'Assets/Pictures/two/Pepe Sunset.png', alt: 'model', title: 'model'}
+];
+
+
+
+function populateThree() {
+    const threeContainer = document.getElementById('three-Container');
+
+    threeImages.forEach(i => {
+        const threeWrapper = document.createElement('div'); //add div
+        threeWrapper.classList.add('portfolioItem') // styling
+        const img = document.createElement('img');
+        img.src = i.src;
+        img.alt = i.alt;
+
+        threeWrapper.appendChild(img);
+        threeContainer.appendChild(threeWrapper);
+        
+    });
+}
+function populateTwo() {
+    const twoContainer = document.getElementById('two-Container');
+
+    twoImages.forEach(i => {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('portfolioItem');
+
+        const img = document.createElement('img');
+        img.src = i.src;
+        img.alt = i.alt;
+
+        wrapper.appendChild(img);
+        twoContainer.appendChild(wrapper);
+    });
+}
+
+populateTwo();
+populateThree();
